1217917379cb26a529aff70cb6e5a535408f4240
Anton Sankov <a.sankov96@gmail.com>
2022-11-20 23:57:33 +0200

    Add some comments

--- hello.bpf.c
+++ hello.bpf.c
@@ -4,6 +4,10 @@
 SEC("kprobe/sys_execve")
 int hello(void *ctx)
 {
+    // write some data to some well-known location
+    // this function is useful only for debugging
+    // if used in production can conflict with other
+    // ebpf programs writing to the same pipe
     bpf_printk("Hello, Go");
     return 0;
 }
